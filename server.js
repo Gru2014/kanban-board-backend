c6297b44cfc4f4794f5d670eab360cb038f7eb8b
Emanuel <emanueldeaguiar99@gmail.com>
2024-11-27 15:39:18 -0500

    kanban-board-backend

---
+++ server.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+});
+
+const users = {};
+const tasks = {};
+
+const getInitialUsers = () => Object.keys(users).map((id) => users[id]);
+const getInitialTasks = () => Object.keys(tasks).map((id) => tasks[id]);
+
+let initialUserTemp = [];
+let initialTaskTemp = [];
+
+const updateUsers = () => {
+  initialUserTemp = getInitialUsers();
+};
+
+const updateTasks = () => {
+  initialTaskTemp = getInitialTasks();
+};
+
+io.on("connection", (socket) => {
+  users[socket.id] = { id: socket.id, status: "none", taskId: null };
+  updateUsers();
+  updateTasks();
+
+  io.emit("users-updated", {
+    type: "added",
+    id: socket.id,
+    user: users[socket.id],
+  });
+
+  socket.emit("first-message", {
+    users: initialUserTemp,
+    tasks: initialTaskTemp,
+  });
+
+  socket.on("focus-task", ({ status, taskId }) => {
+    users[socket.id] = {
+      id: socket.id,
+      status: status,
+      taskId: taskId,
+    };
+    io.emit("users-updated", {
+      type: "updated",
+      id: socket.id,
+      user: users[socket.id],
+    });
+    updateUsers();
+  });
+
+  socket.on("delete-task", (id) => {
+    delete tasks[id];
+    io.emit("task-deleted", id);
+    updateTasks();
+  });
+
+  socket.on("update-task", (task) => {
+    tasks[task.id] = task;
+    io.emit("task-updated", task);
+    updateTasks();
+  });
+
+  socket.on("add-task", (task) => {
+    tasks[task.id] = task;
+    io.emit("task-added", task);
+    updateTasks();
+  });
+
+  socket.on("move-task", (data) => {
+    if (!data) return;
+    const { id, newStatus } = data;
+    if (id && newStatus) {
+      tasks[id].status = newStatus;
+      users[socket.id].taskId = null;
+      users[socket.id].status = "none";
+      io.emit("task-moved", { id, newStatus, user: users[socket.id] });
+    }
+    updateTasks();
+    updateUsers();
+  });
+
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+    io.emit("users-updated", {
+      type: "removed",
+      id: socket.id,
+      user: users[socket.id],
+    });
+    updateUsers();
+  });
+});
+
+server.listen(4000, () =>
+  console.log("Server running on http://localhost:4000")
+);
